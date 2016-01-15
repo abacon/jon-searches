f43a47c91eec7f96fb9e6c3f1bff53210e2829c1
Andrew Bacon <abacon@gmail.com>
2016-01-14 16:49:43 -0800

    Better error reporting and some cleanup.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ sentry.captureMessage('Server started.', {level: 'info'})
 let T = new Twit(creds)
 let jonId = '52937207'
 let xtrasearchesId = '4798595181'
-let jonTweets = T.stream('user', {follows: [jonId, xtrasearchesId].join(',')})
+let stream = T.stream('user', {follows: [jonId, xtrasearchesId].join(',')})
 
 let reply = function (id, searchURL, imagePath) {
   var b64content = fs.readFileSync(imagePath, { encoding: 'base64' })
@@ -28,32 +28,32 @@ let reply = function (id, searchURL, imagePath) {
 
     // now we can reference the media and post a tweet (media will attach to the tweet)
     var mediaIdStr = data.media_id_string
-    var params = { status: '@xtratoppings ' + searchURL, in_reply_to_status_id: id, media_ids: [mediaIdStr] }
+    var params = { status: '@xtrasearches ' + searchURL, in_reply_to_status_id: id, media_ids: [mediaIdStr] }
 
     T.post('statuses/update', params, function (err, data, response) {
       if (err) {
         sentry.captureError(new Error(err))
         return
       }
-      sentry.captureMessage('Posted a reply to tweet ' + id)
+      sentry.captureMessage('Replied to tweet', {level: 'info', extra: {id: id, url: searchURL, imagePath: imagePath}})
     })
   })
 }
 
 let handleTweet = function (tweet) {
   if (!tweet.retweeted && isSearch(tweet.text)) {
-    sentry.captureMessage('search tweet', {level: 'info', extra: {id: tweet.id_str, status: tweet.text}})
+    sentry.captureMessage('Search tweet', {level: 'info', extra: {id: tweet.id_str, status: tweet.text}})
     let url = makeQuery(tweet.text)
     screenshot(url, tweet.id_str, reply)
   } else {
-    sentry.captureMessage(`non-search tweet. `, {level: 'info', extra: {id: tweet.id_str, status: tweet.text}})
+    sentry.captureMessage('Non-search tweet', {level: 'info', extra: {id: tweet.id_str, status: tweet.text}})
   }
 }
 
 let logEvent = function logEvent () {
   console.log(arguments)
-  sentry.captureMessage('Got a message', {extra: arguments, level: 'info'})
+  sentry.captureMessage('Message', {extra: arguments, level: 'info'})
 }
 
-jonTweets.on('tweet', handleTweet)
-jonTweets.on('message', logEvent)
+stream.on('tweet', handleTweet)
+stream.on('message', logEvent)
